c26c3ea31f9d3b772a02430705c402712e020aa9
Mateo <mateobastidas146@gmail.com>
2026-05-27 17:14:55 -0300

    Configuración de variables de entorno para Firebase

--- servidor.js
+++ servidor.js
@@ -3,9 +3,20 @@ const admin = require("firebase-admin");
 const path = require("path");
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
+const admin = require("firebase-admin");
 
-// Inicializá Firebase Admin con tus credenciales
-const serviceAccount = require("./config/pptonline-ed145-firebase-adminsdk-fbsvc-8f2ed5aa68.json");
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Asegúrate de formatear correctamente
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
